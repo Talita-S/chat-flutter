06e9de34bb9571cffea0521c918b487c0ffd5efb
Talita-S <talitassantos2015@gmail.com>
2022-02-03 13:45:58 -0300

    resolvendo erro gradle e testando conexão com firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+// @dart=2.9
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+  Firestore.instance.collection("col").document("doc").setData(
+      {"texto": "teste"});
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
